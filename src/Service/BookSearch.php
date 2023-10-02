c526ddcc309ef94ab28c99fb3632b44d776f31d7
Sergio Mendolia <sergio@mendolia.dev>
2023-10-02 16:56:56 +0200

    Enable typsense search

---
+++ src/Service/BookSearch.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Service;
+
+use ACSEO\TypesenseBundle\Finder\SpecificCollectionFinder;
+
+/**
+ * @phpstan-type MetadataType array{ title:string, authors: BookAuthor[], main_author: ?BookAuthor, description: ?string, publisher: ?string, publish_date: ?\DateTime, language: ?string, tags: string[], serie:?string, serie_index: ?int, cover: ?EbookCover }
+ */
+class BookSearch
+{
+    public function __construct(private SpecificCollectionFinder $autocompleteBookFinder)
+    {
+    }
+
+    public function autocomplete(string $query): array
+    {
+        return $this->autocompleteBookFinder->search($query)->getResults();
+    }
+}
