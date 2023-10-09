cde8710e546e369e969daaf513c699c183dcaccb
Sergio Mendolia <sergio@mendolia.dev>
2023-10-09 14:27:17 +0200

    longer page on shelves

--- src/Controller/ShelfController.php
+++ src/Controller/ShelfController.php
@@ -13,7 +13,7 @@ class ShelfController extends AbstractController
     #[Route('/shelf/{slug}/{page}', name: 'app_shelf', requirements: ['page' => '\d+'])]
     public function index(Shelf $shelf, PaginatorInterface $paginator, int $page = 1): Response
     {
-        $pagination = $paginator->paginate($shelf->getBooks(), $page, 10);
+        $pagination = $paginator->paginate($shelf->getBooks(), $page, 60);
 
         return $this->render('shelf/index.html.twig', [
             'shelf' => $shelf,
