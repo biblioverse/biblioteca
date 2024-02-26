721268da0694eb9775ef05beadba6838f0ac9d25
Sergio Mendolia <sergio@mendolia.dev>
2024-02-26 18:30:53 +0100

    fix again

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,6 @@ import './bootstrap.js';
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/global.scss';
 
 require('bootstrap');
