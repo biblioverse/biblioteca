a129dbe963cda8dcaebb1b14fdb42b8f3c22ce01
Laurent Constantin <laurent.constantin@liip.ch>
2024-05-31 17:35:45 +0200

    feat(kobo): KoboDevice admin is accessible to all users

--- src/Form/KoboType.php
+++ src/Form/KoboType.php
@@ -6,12 +6,17 @@ use App\Entity\KoboDevice;
 use App\Entity\Shelf;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class KoboType extends AbstractType
 {
+    public function __construct(protected Security $security)
+    {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -20,18 +25,20 @@ class KoboType extends AbstractType
             ->add('forceSync', null, [
                 'label' => 'Force Sync',
                 'required' => false,
-            ])
-            ->add('user', EntityType::class, [
+            ]);
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            $builder->add('user', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'username',
-            ])
-            ->add('shelves', EntityType::class, [
-                'label' => 'Sync with Shelves',
-                'class' => Shelf::class,
-                'choice_label' => 'name',
-                'multiple' => true,
-                'expanded' => true,
-            ])
+            ]);
+        }
+        $builder->add('shelves', EntityType::class, [
+            'label' => 'Sync with Shelves',
+            'class' => Shelf::class,
+            'choice_label' => 'name',
+            'multiple' => true,
+            'expanded' => true,
+        ])
         ;
     }
 
