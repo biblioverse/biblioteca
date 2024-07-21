8d2df18388ed77aa06ebda54ee58da83549ae625
Sergio Mendolia <sergio@mendolia.dev>
2024-07-21 14:05:10 +0200

    Add default

--- src/EventListener/LoginListener.php
+++ src/EventListener/LoginListener.php
@@ -5,6 +5,7 @@ namespace App\EventListener;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Event\AuthenticationSuccessEvent;
 
@@ -25,6 +26,10 @@ final class LoginListener
         $user->setLastLogin(new \DateTime());
         $this->entityManager->persist($user);
         $this->entityManager->flush();
+
+        if (!$this->requestStack->getCurrentRequest() instanceof Request || !$this->requestStack->getCurrentRequest()->hasSession()) {
+            return;
+        }
 
         $this->requestStack->getSession()->set('_locale', $user->getLanguage() ?? 'en');
     }
