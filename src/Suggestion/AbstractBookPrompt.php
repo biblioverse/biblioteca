17aa04e908eeccb912852184174388efd7f78c03
Laurent Constantin <laurent.constantin@liip.ch>
2024-08-01 18:49:24 +0200

    feat(book): Uniform prompt for openAi

---
+++ src/Suggestion/AbstractBookPrompt.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Suggestion;
+
+use App\Entity\Book;
+use App\Entity\User;
+
+abstract class AbstractBookPrompt
+{
+    protected function replaceBookOccurrence(Book $book, string $prompt): string
+    {
+        $bookString = '"'.$book->getTitle().'" by '.implode(' and ', $book->getAuthors());
+
+        if ($book->getSerie() !== null) {
+            $bookString .= ' number '.$book->getSerieIndex().' in the series "'.$book->getSerie().'"';
+        }
+
+        return str_replace('{book}', $bookString, $prompt);
+    }
+
+    abstract public function getPrompt(Book $book, User $user): string;
+}
