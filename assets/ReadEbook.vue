e52845c6449c66e8ab128da99f62151c51bd4ec7
Laurent Constantin <laurent.constantin@liip.ch>
2024-08-13 01:08:05 +0200

    feature(book): Save reading progression from web

--- assets/ReadEbook.vue
+++ assets/ReadEbook.vue
@@ -12,18 +12,59 @@
 </template>
 
 <script setup>
-const props = defineProps({'file': String, 'css': String, 'bgColor': String})
+const props = defineProps({
+  'file': String,
+  'css': String,
+  'bgColor': String,
+  'percent': String,
+  'progressionUrl': String
+})
 import { VueReader } from 'vue-book-reader'
 const initialCfi = new URLSearchParams(window.location.search).get('cfi');
 
-const locationChange = (detail) => {
+function debounce(func, delay) {
+  let timeoutId;
+  return function(...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(this, args), delay);
+  };
+}
+
+const locationChange = debounce((detail) => {
   const { fraction } = detail
   console.log('locationChange', fraction, detail)
   history.pushState({fraction: fraction, }, document.title, "?cfi=" + encodeURIComponent(detail.cfi) + "#page="+ detail.location.current+"&percent=" + (fraction * 100).toFixed(2));
-}
+
+  if(props.progressionUrl !== ""){
+    fetch(props.progressionUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({percent: fraction, cfi: detail.cfi}),
+    })
+    .then(response => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
+}, 500);
 
 const getInitialLocation = () => {
-    return initialCfi ?? null
+    // If we have a cfi in the url we use it
+    if(initialCfi){
+      return initialCfi
+    }
+    // If we know the current progress in percent we use it
+    if(props.percent !== "undefined"){
+      const percent = parseFloat(props.percent)
+      if(!isNaN(percent)){
+        return {fraction: percent};
+      }
+    }
+
+    return null;
 }
 
 const getRendition = async (rendition) => {
--- assets/read-ebook.js
+++ assets/read-ebook.js
@@ -6,5 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const file = document.getElementById(mountId).getAttribute('data-file')
     const css = document.getElementById(mountId).getAttribute('data-css')
     const bgColor = document.getElementById(mountId).getAttribute('data-background-color')
-    createApp(ReadEBook, {file: file, css: css, bgColor: bgColor}).mount(`#${mountId}`);
+    const percent = document.getElementById(mountId).getAttribute('data-percent')
+    const progressionUrl = document.getElementById(mountId).getAttribute('data-progressionUrl')
+    createApp(ReadEBook, {
+        file: file,
+        css: css,
+        bgColor: bgColor,
+        percent: percent,
+        progressionUrl: progressionUrl
+    }).mount(`#${mountId}`);
 });
