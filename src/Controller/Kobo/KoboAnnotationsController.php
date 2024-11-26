42dcb430453e23672d9b04bdb0ecc084e9d05978
Sergio Mendolia <sergio@mendolia.dev>
2024-11-26 21:47:52 +0100

    Fix offenses

--- src/Controller/Kobo/KoboAnnotationsController.php
+++ src/Controller/Kobo/KoboAnnotationsController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Kobo;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,7 +13,7 @@ class KoboAnnotationsController extends AbstractKoboController
      * Update reading state.
      **/
     #[Route('/content/{uuid}/annotations', name: 'api_v3_annotations', requirements: ['uuid' => '^[a-zA-Z0-9\-]+$'])]
-    public function state(string $uuid, Request $request): Response|JsonResponse
+    public function state(string $uuid): Response|JsonResponse
     {
         return new JsonResponse([]);
     }
