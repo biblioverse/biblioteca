bce4bba8a74827d7c2d19d6e0b2a4531d20ae2cb
Sergio Mendolia <sergio@mendolia.dev>
2024-12-01 12:18:20 +0100

    Add dry run mode to relocation command

--- src/Command/BooksRelocateCommand.php
+++ src/Command/BooksRelocateCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -32,6 +33,7 @@ class BooksRelocateCommand extends Command
 
     protected function configure(): void
     {
+        $this->addOption('force', 'f', InputOption::VALUE_NONE, 'Execute the relocation. By default only a dry run is done');
     }
 
     /**
@@ -43,26 +45,47 @@ class BooksRelocateCommand extends Command
 
         $allBooks = $this->bookRepository->findAll();
 
+        $executeChanges = (bool) $input->getOption('force');
+
+        if (!$executeChanges) {
+            $io->warning('This is a dry run. Use --force to execute the relocation');
+        }
+
         $progressBar = new ProgressBar($output, count($allBooks));
         $progressBar->start();
+        $relocationCounter = 0;
         foreach ($allBooks as $book) {
             $progressBar->advance();
             try {
-                $book = $this->fileSystemManager->renameFiles($book);
+                $calculatedPath = $this->fileSystemManager->getCalculatedFilePath($book, false).$this->fileSystemManager->getCalculatedFileName($book);
+                $needsRelocation = $this->fileSystemManager->getCalculatedFilePath($book, false) !== $book->getBookPath();
+
+                if ($needsRelocation) {
+                    $relocationCounter++;
+
+                    $io->writeln('Book '.$book->getTitle().': '.$book->getBookPath().'=>'.$calculatedPath);
+                }
+                if ($executeChanges) {
+                    $book = $this->fileSystemManager->renameFiles($book);
+                }
                 $this->entityManager->persist($book);
             } catch (\Exception $e) {
                 $io->error($e->getMessage());
                 continue;
             }
         }
-        $this->entityManager->flush();
-
+        $io->success('Relocated '.$relocationCounter.' books');
+        if ($executeChanges) {
+            $this->entityManager->flush();
+        }
         $progressBar->finish();
 
-        $io->writeln('');
-        $io->writeln('Remove empty folders');
-        $this->fileSystemManager->removeEmptySubFolders($this->fileSystemManager->getBooksDirectory());
-        $this->fileSystemManager->removeEmptySubFolders($this->fileSystemManager->getCoverDirectory());
+        if ($executeChanges) {
+            $io->writeln('');
+            $io->writeln('Remove empty folders');
+            $this->fileSystemManager->removeEmptySubFolders($this->fileSystemManager->getBooksDirectory());
+            $this->fileSystemManager->removeEmptySubFolders($this->fileSystemManager->getCoverDirectory());
+        }
 
         return Command::SUCCESS;
     }
