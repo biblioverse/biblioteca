dfa4d39288c6883a8fc3bb50c7f209bea2e82f61
Sergio Mendolia <sergio@mendolia.dev>
2024-12-19 07:27:58 +0100

    Implement error handling and toast notifications for relocation
    Added error handling in the book relocation process to provide user feedback on success or failure. Migrated flash messages from alerts to Bootstrap toasts for enhanced user experience. Updated PHP requirement to 8.2 in composer.json.
    
    Signed-off-by: Sergio Mendolia <sergio@mendolia.dev>

--- assets/app.js
+++ assets/app.js
@@ -5,13 +5,16 @@ import './bootstrap.js';
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/global.scss';
 
-require('bootstrap');
+//const boostrap = require('bootstrap');
+import * as bootstrap from 'bootstrap'
 
 window.addEventListener('manager:flush', () => {
     location.reload()
 
 });
+
+const toastElList = document.querySelectorAll('.toast')
+const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl).show())
