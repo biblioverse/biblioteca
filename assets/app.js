117f29c9f5ccd68fb4af508cf21b0a0d1eec0292
Sergio Mendolia <sergio@mendolia.dev>
2024-12-19 07:39:35 +0100

    "Clean up app.js by removing comments and unused code"

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,10 @@
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/global.scss';
-
-//const boostrap = require('bootstrap');
 import * as bootstrap from 'bootstrap'
+
 
 window.addEventListener('manager:flush', () => {
     location.reload()
-
 });
 
 const toastElList = document.querySelectorAll('.toast')
