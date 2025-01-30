7b8c43c83663e4595d095a8c0fbe2dcda5564f80
Laurent Constantin <laurent.constantin@liip.ch>
2025-01-30 11:21:47 +0100

    Interface typo

---
+++ src/Ai/Context/ContextBuildingInteface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Ai\Context;
+
+/**
+ * @deprecated Use ContextBuildingInterface instead (with a r)
+ * @see ContextBuildingInterface
+ */
+interface ContextBuildingInteface extends ContextBuildingInterface
+{
+}
