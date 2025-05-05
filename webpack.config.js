853cc9c2a89b4e7bc67d8c284e2c38096691de0d
Stéphane Goetz <stephane.goetz@onigoetz.ch>
2025-05-05 13:02:24 +0200

    New login page

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('login', './assets/login.js')
     .addEntry('read-ebook', './assets/read-ebook.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
