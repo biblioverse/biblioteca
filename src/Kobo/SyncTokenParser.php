04979e437067b09564c8ccc80d35e66b8df1018a
Laurent Constantin <laurent.constantin@liip.ch>
2025-07-10 12:30:39 +0200

    Fix: Kobo Sync token format changed

--- src/Kobo/SyncTokenParser.php
+++ src/Kobo/SyncTokenParser.php
@@ -13,8 +13,20 @@ class SyncTokenParser
             return new SyncToken();
         }
 
-        $infos = (array) json_decode((string) base64_decode($rawToken, true), true, 512, JSON_THROW_ON_ERROR);
+        $data = null;
+        $version = null;
+        $b64decoded = base64_decode($rawToken, false);
+        if ($b64decoded === false) {
+            throw new BadRequestHttpException('Invalid base64 token');
+        }
 
+        // New version is {"typ":1,"ver":null,"ptyp":"SyncToken"}{"InternalSyncToken":"..JWT.."}
+        if(str_starts_with($b64decoded, '{"typ":1')){
+            return $this->decodeInternalSyncToken($b64decoded);
+        }
+
+        // Backward compatibility
+        $infos = (array)json_decode((string)$b64decoded, true, 512, JSON_THROW_ON_ERROR);
         /** @var array<string,string|null> $data */
         $data = (array) ($infos['data'] ?? []);
 
@@ -39,6 +51,18 @@ class SyncTokenParser
         }
         try {
             return new \DateTimeImmutable('@'.$timeStamp);
+        } catch (\Exception) {
+            return null;
+        }
+    }
+
+    private function timeStampISO8601(?string $timeStamp): ?\DateTimeImmutable
+    {
+        if ($timeStamp === null || trim($timeStamp) === '') {
+            return null;
+        }
+        try {
+            return \DateTimeImmutable::createFromFormat('Y-m-d\TH:i:s\Z', $timeStamp);
         } catch (\Exception) {
             return null;
         }
@@ -69,5 +93,42 @@ class SyncTokenParser
         } catch (\Throwable $e) {
             throw new BadRequestHttpException($e->getMessage(), $e);
         }
+    }
+
+    private function decodeInternalSyncToken(string $b64decoded): SyncToken
+    {
+        $pos = strpos($b64decoded, '}{');
+        $b64decoded = substr($b64decoded, $pos + 1);
+        $secondPartData = json_decode((string) $b64decoded, true, 512, JSON_THROW_ON_ERROR);
+        if(!isset($secondPartData['InternalSyncToken'])) {
+            throw new BadRequestHttpException('Invalid base64 token: No InternalSyncToken');
+        }
+
+        $jwtToken = $secondPartData['InternalSyncToken'];
+        $parts = explode('.', (string)$jwtToken);
+        if(count($parts) < 2){
+            throw new BadRequestHttpException('Invalid JWT token: '. count($parts). " parts");
+        }
+        $payload = base64_decode(strtr($parts[1], '-_', '+/'));
+        $version= "SyncToken2Parts";
+        $data = json_decode((string)$payload, true, 512, JSON_THROW_ON_ERROR);
+
+        $timestampSubscriptionEntitlements = $this->timeStampISO8601($data['SubscriptionEntitlements']["Timestamp"] ?? null);
+        $timestampEntitlements = $this->timeStampISO8601($data['Entitlements']["Timestamp"] ?? null);
+        $timestampDeletedEntitlements = $this->timeStampISO8601($data['DeletedEntitlements']["Timestamp"] ?? null);
+        $timestampReadingStates = $this->timeStampISO8601($data['ReadingStates']["Timestamp"] ?? null);
+        $timestampTags = $this->timeStampISO8601($data['Tags']["Timestamp"] ?? null);
+        $timestampDeletedTags = $this->timeStampISO8601($data['DeletedTags']["Timestamp"] ?? null);
+        $timestampProductMetadata = $this->timeStampISO8601($data['ProductMetadata']["Timestamp"] ?? null);
+
+        $token = new SyncToken();
+        $token->version = "InternalSyncToken";
+        $token->lastModified = $timestampEntitlements;
+        $token->lastCreated = null;
+        $token->archiveLastModified = null;
+        $token->readingStateLastModified = $timestampReadingStates;
+        $token->tagLastModified = null;
+
+        return $token;
     }
 }
