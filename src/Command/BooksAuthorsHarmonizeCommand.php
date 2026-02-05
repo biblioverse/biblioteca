838e7a38b9f79ae955643a89c3e50938a9d6e045
Laurent FRANCOISE <lfrancoise@gmail.com>
2026-02-05 23:17:08 +0100

    fix: Apply Rector suggestions
    - Use instanceof Command instead of null check
    - Add int type to BATCH_SIZE constants

--- src/Command/BooksAuthorsHarmonizeCommand.php
+++ src/Command/BooksAuthorsHarmonizeCommand.php
@@ -21,7 +21,7 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class BooksAuthorsHarmonizeCommand extends Command
 {
-    private const BATCH_SIZE = 30;
+    private const int BATCH_SIZE = 30;
 
     public function __construct(
         private readonly EntityManagerInterface $em,
